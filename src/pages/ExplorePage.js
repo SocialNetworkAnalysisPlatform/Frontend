0c25d8c60d70eb50f5509a6b5ac51e284a34c0ac
Linoy <linoychubok@gmail.com>
2022-03-09 14:57:40 +0200

    Add explore

--- src/pages/ExplorePage.js
+++ src/pages/ExplorePage.js
@@ -1,17 +1,59 @@
 
-import React from 'react'
+import React, { useState } from 'react'
 import { Layout } from '../components/Layout'
 import { useAuth } from '../contexts/AuthContext'
 
-import { Typography } from '@mui/material';
 import Box from '@mui/material/Box';
 import Stack from '@mui/material/Stack';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import VisibilityTwoToneIcon from '@mui/icons-material/VisibilityTwoTone';
 
 const ExplorePage = () => {
+
+  const [cards, setCards] = useState([
+    { id: 1, title: 'Project', description: 'description', img: '', views: 123  },
+    { id: 2, title: 'Project', description: 'description', img: '', views: 123  },
+    { id: 3, title: 'Project', description: 'description', img: '', views: 123  },
+    { id: 4, title: 'Project', description: 'description', img: '', views: 123  },
+    { id: 5, title: 'Project', description: 'description', img: '', views: 123  },
+    { id: 6, title: 'Project', description: 'description', img: '', views: 123  },
+  ]);
+
   return (
     <Layout>
-      <div>explore</div>
-
+      <Typography sx={{ fontSize: 24, fontWeight: 500, color: "#6366f1" }}>Explore</Typography>
+      <Typography variant="body2" color="text.secondary">Explore Visualizations Published by the Community</Typography>
+      <Stack sx={{ display: 'flex', flexWrap: 'wrap', flexDirection: 'row', justifyContent: 'flex-start', mt: 3 }}>
+        { cards.map((card, index) => {
+          return (
+            <Card key={card.id} index={index} sx={{ width: '28%', mr: 5, mb: 5 }}>
+            <CardMedia
+              component="img"
+              height="140"
+              image={card.img}
+              alt={card.title}
+            />
+            <CardContent>
+              <Typography gutterBottom variant="h5" component="div">
+                {card.title}
+              </Typography>
+              <Typography variant="body2" color="text.secondary">
+                {card.description}
+              </Typography>
+            </CardContent>
+            <CardActions>
+              <VisibilityTwoToneIcon sx={{ color: '#6366f1'}}  />
+              <Typography sx={{ ml: 1 }} variant="body2" color="text.secondary">121</Typography>
+            </CardActions>
+          </Card>
+         )
+        })}
+      </Stack>
     </Layout>
   )
 }
--- src/pages/NewConversationPage.js
+++ src/pages/NewConversationPage.js
@@ -126,7 +126,7 @@ const NewConversationPage = (props) => {
             <Typography sx={{ fontSize: 18, fontWeight: 500, color: "#6366f1" }}>Create a new conversation</Typography>
 
             <Paper  elevation={2}>
-              <Stack spacing={4} spacing={1} p={2}>
+              <Stack spacing={4} p={2}>
                 <FormControl>
                   <FormLabel sx={{ color: '#000000DE', fontSize: 14, fontWeight: 500 }}>Title</FormLabel>
                   <OutlinedInput size="small" sx={{ width: 300,  }} required value={newConversation.title} onChange={(e) => setNewConversation({...newConversation, title: e.target.value}) }/>

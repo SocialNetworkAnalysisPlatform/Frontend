4c10277f2d27106b14aedf5863147350ade6a6bc
Linoy <linoychubok@gmail.com>
2022-05-16 23:45:43 +0300

    Add skeleton explore card

--- src/pages/ExplorePage.js
+++ src/pages/ExplorePage.js
@@ -12,6 +12,7 @@ import CardMedia from '@mui/material/CardMedia';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 import VisibilityTwoToneIcon from '@mui/icons-material/VisibilityTwoTone';
+import SkeletonExploreCard from '../skeletons/SkeletonExploreCard'
 
 const ExplorePage = () => {
 
@@ -24,6 +25,8 @@ const ExplorePage = () => {
     { id: 6, title: 'Project', description: 'description', img: '', views: 123  },
   ]);
 
+  const [loading, setLoading] = useState(true);
+
     // Scroll to top on page load
     useEffect(() => {
       window.scrollTo(0, 0)
@@ -35,30 +38,29 @@ const ExplorePage = () => {
       <Typography sx={{ fontSize: 24, fontWeight: 500, color: "#6366f1" }}>Explore</Typography>
       <Typography variant="body2" color="text.secondary">Explore Visualizations Published by the Community</Typography>
       <Stack sx={{ display: 'flex', flexWrap: 'wrap', flexDirection: 'row', justifyContent: 'flex-start', mt: 3 }}>
-        { cards.map((card, index) => {
-          return (
-            <Card key={card.id} index={index} sx={{ width: '28%', mr: 5, mb: 5 }}>
-            {/* <CardMedia
-              component="img"
-              height="140"
-              image={card.img}
-              alt={card.title}
-            /> */}
-            <CardContent>
-              <Typography gutterBottom variant="h5" component="div">
-                {card.title}
-              </Typography>
-              <Typography variant="body2" color="text.secondary">
-                {card.description}
-              </Typography>
-            </CardContent>
-            <CardActions>
-              <VisibilityTwoToneIcon sx={{ color: '#6366f1'}}  />
-              <Typography sx={{ ml: 1 }} variant="body2" color="text.secondary">121</Typography>
-            </CardActions>
-          </Card>
-         )
-        })}
+        {
+          loading === false ?
+          cards.map((card, index) => {
+            return (
+              <Card key={card.id} index={index} sx={{ width: '28%', mr: 5, mb: 5 }}>
+                <CardContent sx={{ height: 150}}>
+                  <Typography gutterBottom variant="h5" component="div">
+                    {card.title}
+                  </Typography>
+                  <Typography variant="body2" color="text.secondary">
+                    {card.description}
+                  </Typography>
+                </CardContent>
+                <CardActions>
+                  <VisibilityTwoToneIcon sx={{ color: '#6366f1'}}  />
+                  <Typography sx={{ ml: 1 }} variant="body2" color="text.secondary">121</Typography>
+                </CardActions>
+            </Card>
+          )
+          })
+          :
+          [1,2,3,4,5,6].map((num) => <SkeletonExploreCard/>)
+        }
       </Stack>
     </Layout>
   )
